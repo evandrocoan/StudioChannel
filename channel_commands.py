ea66a63a6facb53e95670e37fff31b40128e57dd
evandrocoan <evandrocoan@hotmail.com>
2017-10-22 15:40:02 -0200

    Implemented the command MyChannel: Select Package to Update Git Tag

--- channel_commands.py
+++ channel_commands.py
@@ -75,9 +75,9 @@ class StudioChannelRunUninstallationWizard( sublime_plugin.ApplicationCommand ):
 
 class StudioChannelGenerateChannelFile( sublime_plugin.ApplicationCommand ):
 
-    def run(self, create_tags):
+    def run(self, create_tags, command="all"):
         sublime.active_window().run_command( "show_panel", {"panel": "console", "toggle": False} )
-        manager_main( settings.g_channel_settings, create_tags )
+        manager_main( settings.g_channel_settings, create_tags, command )
 
 
 class StudioChannelRun( sublime_plugin.ApplicationCommand ):
