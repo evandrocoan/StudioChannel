7be1b98b9d5128cb1269dd6eacd8cc44b3e5b957
evandrocoan <evandrocoan@hotmail.com>
2018-10-17 14:51:27 -0300

    Created the basic structure for Unit Tests

---
+++ tests/testing/main_unit_tests.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+####################### Licensing #######################################################
+#
+#   Copyright 2018 @ Evandro Coan
+#   Project Unit Tests
+#
+#  Redistributions of source code must retain the above
+#  copyright notice, this list of conditions and the
+#  following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above
+#  copyright notice, this list of conditions and the following
+#  disclaimer in the documentation and/or other materials
+#  provided with the distribution.
+#
+#  Neither the name Evandro Coan nor the names of any
+#  contributors may be used to endorse or promote products
+#  derived from this software without specific prior written
+#  permission.
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the
+#  Free Software Foundation; either version 3 of the License, or ( at
+#  your option ) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################################
+#
+
+import os
+import sys
+
+import unittest
+import sublime_plugin
+
+from debug_tools import testing_utilities
+from StudioChannel import commands
+
+
+# from ChannelManager import studio_installer; studio_installer.main("development")
+# from ChannelManager import studio_installer; studio_installer.main("stable")
+# from ChannelManager import studio_uninstaller; studio_uninstaller.main()
+# from StudioChannel import installation_wizard; installation_wizard.install()
+class MainUnitTests(testing_utilities.TestingUtilities):
+
+    def setUp(self):
+        commands.load_channel_settings()
+        commands.load_installation_details()
+        is_channel_installed = commands.is_channel_installed()
+
+        if commands.is_channel_installed():
+            self.fail("You cannot run the installation test when the channel is already installed! (%s)" % is_channel_installed)
+
+    def test_installationAndUninstallationStable(self):
+        pass
+
+    def test_installationAndUninstallationDevelopment(self):
+        pass
+
+    def test_upgradeStable(self):
+        pass
+
+    def test_upgradeDevelopment(self):
+        pass
+
+    def test_downgradeStable(self):
+        pass
+
+    def test_downgradeDevelopment(self):
+        pass
+
